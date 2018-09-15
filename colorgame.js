5c0dd0ce4685f3550c424fb3eb652e769efd371c
rbhansali9 <rbhansali9@gatech.edu>
2018-09-15 03:05:58 -0400

    added few more functionalities

--- colorgame.js
+++ colorgame.js
@@ -1,9 +1,4 @@
-/*var colors = ["rgb(255, 0, 0)",
-				"rgb(255, 255, 0)",
-				"rgb(0, 255, 0)",
-				"rgb(255, 0, 0)",
-				"rgb(0, 255, 255)",
-				"rgb(255, 0, 255)",]*/
+var colors = [];
 
 function randomColorGenerator(){
 	var rNum = Math.floor(Math.random() * 255 + 1);
@@ -13,7 +8,50 @@ function randomColorGenerator(){
 }
 
 var squares = document.querySelectorAll(".square");
+var colorDisplay =document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
-for(var i = 0;i<=squares.length-2;i++){
-	squares[i].style.backgroundColor = randomColorGenerator();
+//toDO
+
+var pickedColor = null;
+
+
+for(var i = 0;i<=squares.length-1;i++){
+	//add randomColors
+	var randomColor =  randomColorGenerator();
+	squares[i].style.backgroundColor = randomColor;
+	colors.push(randomColor);
+
+	//add click listeners to squares
+	squares[i].addEventListener("click",function(){
+		//grab color of the clicked square
+		//compare with the picked color
+		var clickedColor = this.style.backgroundColor;
+		if(clickedColor == pickedColor){
+			messageDisplay.textContent = "correct";
+			changeColor(clickedColor);
+		}
+		else{
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	});
 }
+
+pickedColor = pickColor();
+
+
+function changeColor(color){
+	//loop through all the squares
+	for(var i=0;i<=squares.length-1;i++){
+		//change the each color to given color
+		squares[i].style.backgroundColor = color;
+	}	
+}
+
+function pickColor(){
+	var randomIndex = Math.floor(Math.random()*colors.length);
+	return colors[randomIndex];
+}
+
+colorDisplay.textContent = pickedColor;
