2de97976b0b510d836464e5f12a8f60ba8d5a5d5
rbhansali9 <rbhansali9@gatech.edu>
2018-09-15 00:27:09 -0400

    implemented randomColorGenerator() in .js

--- colorgame.js
+++ colorgame.js
@@ -1,13 +1,19 @@
-var colors = ["rgb(255, 0, 0)",
+/*var colors = ["rgb(255, 0, 0)",
 				"rgb(255, 255, 0)",
 				"rgb(0, 255, 0)",
 				"rgb(255, 0, 0)",
 				"rgb(0, 255, 255)",
-				"rgb(255, 0, 255)",]
+				"rgb(255, 0, 255)",]*/
+
+function randomColorGenerator(){
+	var rNum = Math.floor(Math.random() * 255 + 1);
+	var gNum = Math.floor(Math.random() * 255 + 1);
+	var bNum = Math.floor(Math.random() * 255 + 1);
+	return "rgb("+rNum+", "+gNum+", "+bNum+")";
+}
 
 var squares = document.querySelectorAll(".square");
 
 for(var i = 0;i<=squares.length-2;i++){
-	console.log(i);
-	squares[i].style.backgroundColor = colors[i];
+	squares[i].style.backgroundColor = randomColorGenerator();
 }
