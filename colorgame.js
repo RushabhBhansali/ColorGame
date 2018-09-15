3840845dd0ab68261886f46ceb89bfd807354d56
rbhansali9 <rbhansali9@gatech.edu>
2018-09-15 03:43:12 -0400

    reset button implemented

--- colorgame.js
+++ colorgame.js
@@ -3,6 +3,12 @@ var squares = document.querySelectorAll(".square");
 var colorDisplay =document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var pickedColor = null;
+var resetButton = document.querySelector("#reset");
+
+
+setup();
+
 
 function randomColorGenerator(){
 	var rNum = Math.floor(Math.random() * 255 + 1);
@@ -12,13 +18,21 @@ function randomColorGenerator(){
 }
 
 
+resetButton.addEventListener("click",function(){
+	//generate all new colors;
+	//pick a random colors from array;
+	//change colors of squares;
+	colors = [];
+	setup();
+	h1.style.backgroundColor = "";
+	resetButton.textContent = "New Colors";
+	messageDisplay.textContent = "";
+});
 
-//toDO
-
-var pickedColor = null;
 
 
-for(var i = 0;i<=squares.length-1;i++){
+
+/*for(var i = 0;i<=squares.length-1;i++){
 	//add randomColors
 	var randomColor =  randomColorGenerator();
 	squares[i].style.backgroundColor = randomColor;
@@ -42,8 +56,38 @@ for(var i = 0;i<=squares.length-1;i++){
 	});
 }
 
-pickedColor = pickColor();
+pickedColor = pickColor();*/
 
+
+function setup(){
+	for(var i = 0;i<=squares.length-1;i++){
+	//add randomColors
+	var randomColor =  randomColorGenerator();
+	squares[i].style.backgroundColor = randomColor;
+	colors.push(randomColor);
+
+	//add click listeners to squares
+	squares[i].addEventListener("click",function(){
+		//grab color of the clicked square
+		//compare with the picked color
+			var clickedColor = this.style.backgroundColor;
+			if(clickedColor == pickedColor){
+				messageDisplay.textContent = "correct";
+				changeColor(clickedColor);
+				h1.style.backgroundColor = clickedColor;
+				resetButton.textContent = "Play Again";
+
+			}
+			else{
+				this.style.background = "#232323";
+				messageDisplay.textContent = "Try Again";
+			}
+		});
+	}
+
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+}
 
 function changeColor(color){
 	//loop through all the squares
@@ -58,4 +102,3 @@ function pickColor(){
 	return colors[randomIndex];
 }
 
-colorDisplay.textContent = pickedColor;
